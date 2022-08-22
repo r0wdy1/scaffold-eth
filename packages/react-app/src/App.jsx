db5baad61be24f6a1a10678c90da823f06a9be94
dundro <v.a.popov@outlook.com>
2022-08-23 01:56:42 +0300

    init

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -320,7 +320,7 @@ function App(props) {
             */}
 
           <Contract
-            name="YourContract"
+            name="TalentToken"
             price={price}
             signer={userSigner}
             provider={localProvider}
